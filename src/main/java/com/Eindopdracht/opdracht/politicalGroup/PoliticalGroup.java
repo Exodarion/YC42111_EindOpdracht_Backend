aba720d6012ad95fc5a73db200c271295a068415
Exodarion <ExoEmail>
2021-11-19 11:32:55 +0100

    Made PoliticalGroup Entity and Table

--- src/main/java/com/Eindopdracht/opdracht/politicalGroup/PoliticalGroup.java
+++ src/main/java/com/Eindopdracht/opdracht/politicalGroup/PoliticalGroup.java
@@ -1,13 +1,19 @@
 package com.Eindopdracht.opdracht.politicalGroup;
 
 import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import antlr.collections.List;
 
+@Entity
+@Table
 public class PoliticalGroup {
 	String name;
-	//private List<Observer> observers = new ArrayList<Observer>();
 	
 	//make this candidate later
+	//might want to use List<Candidate> members = new ArrayList<Candidate>();
 	private ArrayList<String> members = new ArrayList<String>();
 	PoliticalGroupAlignment pga = PoliticalGroupAlignment.MIDDEN;
 }
