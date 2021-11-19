3cca3b6fd1699450e1d5101fb7ce62090d308298
Eva Bouwman <eva.milou@gmail.com>
2021-11-19 13:21:34 +0100

    Human class en Voter class toegevoeg, met endpoints service repo etc

--- src/main/java/com/Eindopdracht/opdracht/model/Human.java
+++ src/main/java/com/Eindopdracht/opdracht/model/Human.java
@@ -1,5 +1,69 @@
 package com.Eindopdracht.opdracht.model;
 
-public class Human {
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public abstract class Human {
+	
+	@Id
+	@SequenceGenerator(name = "human_sequence", sequenceName = "human_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "human_sequence")
+	@Column(name = "id", updatable = false)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private LocalDate dob;
+	
+	public Human(String firstName, String lastName, LocalDate dob) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+	}
+	
+	public Human() {
+	}
+	
+	public Human(Long id, String firstName, String lastName, LocalDate dob) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Human [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", dob=" + dob + "]";
+	}
 
 }
