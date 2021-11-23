60f76d81ee162321554bd02a44aee6e5e8777371
Eva Bouwman <eva.milou@gmail.com>
2021-11-23 15:35:08 +0100

    kandidaten per partij endpoint gemaakt

--- src/main/java/com/Eindopdracht/opdracht/rest/PoliticalGroupController.java
+++ src/main/java/com/Eindopdracht/opdracht/rest/PoliticalGroupController.java
@@ -3,6 +3,7 @@ package com.Eindopdracht.opdracht.rest;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -36,8 +37,12 @@ public class PoliticalGroupController {
 	
 	@GetMapping (path = "/members/{id}")
 	public List<Candidate> showMembers(@PathVariable long id){
-		PoliticalGroup group = politicalGroupService.findById(id);
-		return politicalGroupService.showMembers(group);
+		// checken of id bestaat
+		Optional <PoliticalGroup> group = politicalGroupService.findById(id);
+		if (group.isPresent())
+		return politicalGroupService.showMembers(group.get());
+		else
+			return null;
 	}
 	
 	@PostMapping ("/add")
