83a280ff7f3790d3a38762425dba538a59ec057b
Eva Bouwman <eva.milou@gmail.com>
2021-12-02 22:58:50 +0100

    verwijderen en veranderen van Thesis voor frontend toegevoegd

--- src/main/java/com/Eindopdracht/opdracht/rest/ThesisController.java
+++ src/main/java/com/Eindopdracht/opdracht/rest/ThesisController.java
@@ -1,11 +1,14 @@
 package com.Eindopdracht.opdracht.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +37,39 @@ public class ThesisController {
 	public List<Thesis> showThesises(){
 		return thesisService.showThesises();
 	}
+	
+	
+	@DeleteMapping(path = "/remove/{id}")
+	public void removeThesis(@RequestBody long id) {
+		if (thesisService.findById(id).isPresent()) {
+			thesisService.removeThesis(id);
+		} 
+			
+	}
+	
+	@PutMapping(path = "/change/{id}/{question}")
+	public void changeThesis(@RequestBody long id, String question) {
+		if (thesisService.findById(id).isPresent()) {
+			thesisService.changeThesis(id, question);
+		}
+	}
+	
+//	@PostMapping(path = "/remove/{id}")
+//	public void removeThesis(@RequestBody long id) {
+//		Optional<Thesis> thesis = thesisService.findById(id);
+//		if (thesis.isPresent())
+//			thesisService.removeThesis(id);
+//		
+//	}
+//	
+//	@PostMapping (path = /change/{id}{question})
+//	
+////	
+//	@PostMapping(path = "/change")
+//		public void changeThesis(@RequestBody Thesis thesis) {
+//		thesisService.changeThesis(thesis);
+//	}
+//	
 	
 
 }
